abe87c3ede18daf39d12513b9b48d78fcf36c32b
kimilgon <moses011010@gmail.com>
2025-06-21 23:14:11 +0900

    firebase-admin 환경변수 인증 방식 변경

--- src/shared/config/firebase-admin.ts
+++ src/shared/config/firebase-admin.ts
@@ -1,8 +1,10 @@
 import admin from 'firebase-admin';
 import { getApps, initializeApp } from 'firebase-admin/app';
 
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!);
+
 const firebaseConfig = {
-  credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS!),
+  credential: admin.credential.cert(serviceAccount),
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,

86fb6b9f5f27c055b64c1272809839e502d37826
kimilgon <moses011010@gmail.com>
2025-07-31 23:15:53 +0900

    feat : 공식 사이트 아니라고 Header에 추가하기 혹시 이 도메인 어떻게 찾아서 들어올까봐..

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -55,138 +55,146 @@ export const Header = () => {
   };
 
   return (
-    <header className="bg-white">
-      <NavigationMenu className="z-50 mx-auto flex h-14 max-w-7xl list-none items-center justify-between px-8">
-        <div className="flex lg:flex-1">
-          <Link className="-m-1.5 p-1.5" href={URLS.HOME}>
-            <span className="sr-only">고운황금손</span>
-            <div className="relative h-8 w-32">
-              <Image alt="home_logo" fill sizes="75" src="/logo_green.png" />
-            </div>
-          </Link>
-        </div>
-        <div className="box-border hidden items-center gap-2 pt-1 lg:flex">
-          <NavigationMenuItem>
-            <NavigationMenuLink href={URLS.COMPANY}>
-              <UlButton enText="Gold Baby" text="고운황금손" />
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenu>
+    <>
+      <div className="text-red sticky top-0 z-50 animate-pulse bg-slate-300 py-3 text-center font-bold text-red-500 duration-500">
+        공식 사이트가 아닌 개발 중인 사이트 입니다.{' '}
+        <Link className="font-normal text-blue-500" href="https://goldbaby.itpage.kr/">
+          공식 사이트 이동하기
+        </Link>
+      </div>
+      <header className="bg-white">
+        <NavigationMenu className="z-50 mx-auto flex h-14 max-w-7xl list-none items-center justify-between px-8">
+          <div className="flex lg:flex-1">
+            <Link className="-m-1.5 p-1.5" href={URLS.HOME}>
+              <span className="sr-only">고운황금손</span>
+              <div className="relative h-8 w-32">
+                <Image alt="home_logo" fill sizes="75" src="/logo_green.png" />
+              </div>
+            </Link>
+          </div>
+          <div className="box-border hidden items-center gap-2 pt-1 lg:flex">
             <NavigationMenuItem>
-              <NavigationMenuTrigger>
-                <UlButton enText="Service" text="산후관리사" />
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.MANAGER.ABOUT}>
-                  산후관리사란?
-                </NavigationMenuLink>
-                <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.MANAGER.WORK}>
-                  산후관리사가 하는 일
-                </NavigationMenuLink>
-                <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.MANAGER.APPLY}>
-                  산후관리사 지원하기
-                </NavigationMenuLink>
-              </NavigationMenuContent>
+              <NavigationMenuLink href={URLS.COMPANY}>
+                <UlButton enText="Gold Baby" text="고운황금손" />
+              </NavigationMenuLink>
             </NavigationMenuItem>
-          </NavigationMenu>
-          <NavigationMenu>
+            <NavigationMenu>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <UlButton enText="Service" text="산후관리사" />
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.MANAGER.ABOUT}>
+                    산후관리사란?
+                  </NavigationMenuLink>
+                  <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.MANAGER.WORK}>
+                    산후관리사가 하는 일
+                  </NavigationMenuLink>
+                  <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.MANAGER.APPLY}>
+                    산후관리사 지원하기
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenu>
+            <NavigationMenu>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <UlButton enText="Price" text="이용안내" />
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.RENTAL}>
+                    대여물품
+                  </NavigationMenuLink>
+                  <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.PRICE}>
+                    이용요금
+                  </NavigationMenuLink>
+                  <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.VOUCHER}>
+                    정부지원바우처
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenu>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>
-                <UlButton enText="Price" text="이용안내" />
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.RENTAL}>
-                  대여물품
-                </NavigationMenuLink>
-                <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.PRICE}>
-                  이용요금
-                </NavigationMenuLink>
-                <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.VOUCHER}>
-                  정부지원바우처
-                </NavigationMenuLink>
-              </NavigationMenuContent>
+              <NavigationMenuLink href={URLS.FRANCHISEE}>
+                <UlButton enText="Franchisee" text="지점안내" />
+              </NavigationMenuLink>
             </NavigationMenuItem>
-          </NavigationMenu>
-          <NavigationMenuItem>
-            <NavigationMenuLink href={URLS.FRANCHISEE}>
-              <UlButton enText="Franchisee" text="지점안내" />
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenu>
+            <NavigationMenu>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <UlButton enText="Consult" text="예약상담" />
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.RESERVATION.APPLY}>
+                    상담신청
+                  </NavigationMenuLink>
+                  <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.RESERVATION.LIST}>
+                    신청목록
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenu>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>
-                <UlButton enText="Consult" text="예약상담" />
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.RESERVATION.APPLY}>
-                  상담신청
-                </NavigationMenuLink>
-                <NavigationMenuLink className="text-sm leading-6 text-gray-900" href={URLS.RESERVATION.LIST}>
-                  신청목록
-                </NavigationMenuLink>
-              </NavigationMenuContent>
+              <NavigationMenuLink href={URLS.REVIEW}>
+                <UlButton enText="Review" text="이용후기" />
+              </NavigationMenuLink>
             </NavigationMenuItem>
-          </NavigationMenu>
-          <NavigationMenuItem>
-            <NavigationMenuLink href={URLS.REVIEW}>
-              <UlButton enText="Review" text="이용후기" />
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              className="absolute right-8 top-2 rounded-full border border-[#0F2E16] px-12 py-2"
-              href={isLinked ? URLS.MYPAGE : isSignedIn ? '/signup' : URLS.LOGIN}
-            >
-              {pending ? '로딩중..' : isLinked ? '마이페이지' : isSignedIn ? '회원가입' : '로그인'}
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a className="text-sm font-semibold leading-6 text-gray-900" href="#">
-            {/* <span aria-hidden="true">&rarr;</span> */}
-          </a>
-        </div>
-        {/* 햄버거 icon, 모바일 헤더 부분 */}
-        <div className="lg:hidden">
-          <Drawer direction="right" open={mobileMenuOpen} onOpenChange={open => setMobileMenuOpen(open)}>
-            <DrawerTrigger>
-              <span className="sr-only">메뉴 열기</span>
-              <svg
-                aria-hidden="true"
-                className="h-6 w-6"
-                fill="none"
-                stroke="black"
-                strokeWidth="1.5"
-                viewBox="0 0 24 24"
+            <NavigationMenuItem>
+              <NavigationMenuLink
+                className="absolute right-8 top-2 rounded-full border border-[#0F2E16] px-12 py-2"
+                href={isLinked ? URLS.MYPAGE : isSignedIn ? '/signup' : URLS.LOGIN}
               >
-                <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
-              </svg>
-            </DrawerTrigger>
-            <DrawerPortal>
-              <DrawerOverlay className="fixed inset-0 bg-black/40" />
-              <DrawerContent className="fixed bottom-0 right-0 flex h-full w-[360px] flex-col rounded-bl-sm rounded-tl-sm bg-white pt-6 lg:hidden">
-                <DrawerHeader>
-                  <div className="h-full flex-1 p-4">
-                    <div className="mx-auto max-w-md">
-                      <DrawerTitle className="mb-4 flex items-center justify-center font-medium">
-                        <div className="relative h-8 w-32">
-                          <Link href={URLS.HOME}>
-                            <Image alt="home_logo" fill sizes="75" src="/logo_green.png" />
-                          </Link>
-                        </div>
-                      </DrawerTitle>
-                      <DrawerDescription className="sr-only">모바일 헤더</DrawerDescription>
-                      <MoblieHeaderContent onChangeMobileMenuOpen={handleChangeMobileMenuOpen} />
+                {pending ? '로딩중..' : isLinked ? '마이페이지' : isSignedIn ? '회원가입' : '로그인'}
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a className="text-sm font-semibold leading-6 text-gray-900" href="#">
+              {/* <span aria-hidden="true">&rarr;</span> */}
+            </a>
+          </div>
+          {/* 햄버거 icon, 모바일 헤더 부분 */}
+          <div className="lg:hidden">
+            <Drawer direction="right" open={mobileMenuOpen} onOpenChange={open => setMobileMenuOpen(open)}>
+              <DrawerTrigger>
+                <span className="sr-only">메뉴 열기</span>
+                <svg
+                  aria-hidden="true"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="black"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              </DrawerTrigger>
+              <DrawerPortal>
+                <DrawerOverlay className="fixed inset-0 bg-black/40" />
+                <DrawerContent className="fixed bottom-0 right-0 flex h-full w-[360px] flex-col rounded-bl-sm rounded-tl-sm bg-white pt-6 lg:hidden">
+                  <DrawerHeader>
+                    <div className="h-full flex-1 p-4">
+                      <div className="mx-auto max-w-md">
+                        <DrawerTitle className="mb-4 flex items-center justify-center font-medium">
+                          <div className="relative h-8 w-32">
+                            <Link href={URLS.HOME}>
+                              <Image alt="home_logo" fill sizes="75" src="/logo_green.png" />
+                            </Link>
+                          </div>
+                        </DrawerTitle>
+                        <DrawerDescription className="sr-only">모바일 헤더</DrawerDescription>
+                        <MoblieHeaderContent onChangeMobileMenuOpen={handleChangeMobileMenuOpen} />
+                      </div>
                     </div>
-                  </div>
-                </DrawerHeader>
-              </DrawerContent>
-            </DrawerPortal>
-          </Drawer>
-        </div>
-      </NavigationMenu>
-      {/* </nav> */}
-    </header>
+                  </DrawerHeader>
+                </DrawerContent>
+              </DrawerPortal>
+            </Drawer>
+          </div>
+        </NavigationMenu>
+        {/* </nav> */}
+      </header>
+    </>
   );
 };
 

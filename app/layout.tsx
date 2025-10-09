83758ba1d452fd8da161ebf0ea0ff523f271e46c
kimilgon <moses011010@gmail.com>
2025-10-09 15:30:21 +0900

    fix : module required제거 모듈 버전 호환성 문제로 프로덕션 빌드가 안됨

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import Script from 'next/script';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { ToastContainer } from 'react-toastify';
@@ -10,24 +11,16 @@ import { ThemeProvider } from '@/src/app/providers/theme-provider';
 import { Footer } from '@/src/widgets/footer';
 import { Header } from '@/src/widgets/header';
 
-let pretendardClass = '';
-
-// nextjs 14 next-font-loader와 HMR 문제로 인해 production 환경에서만 next/font 적용
-if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'production') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-  const localFont = require('next/font/local').default;
-  const pretendard = localFont({
-    src: [
-      { path: './fonts/Pretendard-Regular.woff2', weight: '400', style: 'normal' },
-      { path: './fonts/Pretendard-Medium.woff2', weight: '600', style: 'normal' },
-      { path: './fonts/Pretendard-Bold.woff2', weight: '700', style: 'normal' },
-      { path: './fonts/Pretendard-Black.woff2', weight: '900', style: 'normal' },
-    ],
-    display: 'swap',
-    variable: '--font-pretendard',
-  });
-  pretendardClass = pretendard.variable;
-}
+const pretendard = localFont({
+  src: [
+    { path: './fonts/Pretendard-Regular.woff2', weight: '400', style: 'normal' },
+    { path: './fonts/Pretendard-Medium.woff2', weight: '600', style: 'normal' },
+    { path: './fonts/Pretendard-Bold.woff2', weight: '700', style: 'normal' },
+    { path: './fonts/Pretendard-Black.woff2', weight: '900', style: 'normal' },
+  ],
+  display: 'swap',
+  variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
   title: '고운황금손',
@@ -45,7 +38,7 @@ export default function RootLayout({
   const isDev = process.env.NEXT_PUBLIC_ENVIRONMENT !== 'production';
   return (
     <>
-      <html className={`${pretendardClass}`} lang="en">
+      <html className={`${isDev ? null : pretendard.variable}`} lang="en">
         <body className={`relative`}>
           <ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange enableSystem>
             <NuqsAdapter>

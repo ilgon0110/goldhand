ce0ad064f48dc7517438b01486762504548f1fbb
kimilgon <moses011010@gmail.com>
2026-06-07 20:25:33 +0900

    feat: accessToken 검증 로직 강화

--- app/api/event/detail/route.ts
+++ app/api/event/detail/route.ts
@@ -7,7 +7,7 @@ import type { ICommentData, IEventDetailData } from '@/src/shared/types';
 import { typedJson } from '@/src/shared/utils';
 
 interface IResponseBody {
-  response: 'ok' | 'ng';
+  response: 'ng' | 'ok';
   message: string;
   data: IEventDetailData;
 }

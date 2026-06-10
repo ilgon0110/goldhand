8e3e0bfe8fb310df4db9dc9dd98de00d0fefd93a
kimilgon <moses011010@gmail.com>
2026-06-10 22:21:15 +0900

    style : 문구 및 이용요금 layout 수정

--- app/price/layout.tsx
+++ app/price/layout.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/lib/utils';
+
 export default function PriceLayout({ children }: { children: React.ReactNode }) {
-  return <section className="px-10 md:px-[10vw]">{children}</section>;
+  return <section className={cn('px-6', 'md:px-[10vw]', 'xl:px-[20vw]')}>{children}</section>;
 }
